d9ad2d12f4f79af913b7152d6f53f730b53e72bb
leonx74 <103204803+devleonx74@users.noreply.github.com>
2023-06-30 23:21:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ GatewayIntentBits.Guilds,
 GatewayIntentBits.GuildMembers
 ]})
 
-client.on("ready" => {
+client.on('ready', () => {
 console.log('[+] Discord Bot is Online! '+client.user.username)
-
 })
+
+const welcomeChannelId = 'Welcomechid';
+const leaveChannelId = 'Leavechid';
+
+client.on('guildMemberAdd', (member) => {
+  const welcomeChannel = member.guild.channels.cache.get(welcomeChannelId);
+  if (welcomeChannel) {
+    welcomeChannel.send(`Welcome ${member}`);
+  }
+});
+
+client.on('guildMemberRemove', (member) => {
+  const leaveChannel = member.guild.channels.cache.get(leaveChannelId);
+  if (leaveChannel) {
+    leaveChannel.send(`Goodbye ${member.user.tag}`);
+  }
+});
+
+
 
 
 client.login("your-token")
